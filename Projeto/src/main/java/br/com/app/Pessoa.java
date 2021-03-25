2fc3c52724101fc16646fad3080be8b0d461f74e
Robson <Robson@ROBSON-1516>
2021-03-24 23:09:37 -0300

    Add classe pessoa

---
+++ Projeto/src/main/java/br/com/app/Pessoa.java
@@ -0,0 +1,15 @@
+package br.com.app;
+
+public class Pessoa {
+
+	String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
